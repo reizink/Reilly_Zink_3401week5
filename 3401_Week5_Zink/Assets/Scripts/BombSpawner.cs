c974f869693771d34fe9d61754fbfd8b3b283a04
trippingoverbothfeet <reizink@yahoo.com>
2019-02-12 17:42:00 -0500

    Update UFO Mouse Click & Terrain

---
+++ 3401_Week5_Zink/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour {
+
+    public GameObject BombPrefab;
+    public Transform SpawnPos;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+		if(Input.GetKeyDown (KeyCode.Space)) 
+        {
+            Instantiate(BombPrefab, SpawnPos.position, Quaternion.identity);
+        }
+	}
+}
---
+++ 3401_Week5_Zink/Assets/Scripts/UFOMotor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMotor : MonoBehaviour {
+
+    Vector3 targetPosition;
+
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Input.GetMouseButtonDown (0)) //GetMouseButton vs. GetMouseButtonDown
+        {
+            Ray cameraRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast (cameraRay, out hit))
+            {
+                //Debug.Log(hit.point);
+                targetPosition = new Vector3(hit.point.x, hit.point.y + 5, hit.point.z);
+                //transform.position = targetPosition; // warps
+            }
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 0.1f);
+	}
+
+
+}
