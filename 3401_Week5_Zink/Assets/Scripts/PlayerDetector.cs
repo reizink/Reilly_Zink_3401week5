5e7796d384e2790313152a9d5d51eed75241af7c
trippingoverbothfeet <reizink@yahoo.com>
2019-02-18 22:31:54 -0500

    Add GameScene
    left and right click to move Player to the side.

---
+++ 3401_Week5_Zink/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour {
+
+    public GameObject raycastObject;
+
+    void Start()
+    {
+        //targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = raycastObject.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(raycastObject.transform.position, fwd * 5, Color.green);
+
+        if (Physics.Raycast(raycastObject.transform.position, fwd, out hit, 5))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+        else{
+            Debug.Log("NOTHING AHEAD");
+        }
+    }
+
+}
---
+++ 3401_Week5_Zink/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    // move forward each frame
+    void Update()
+    {
+        transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + .01f);
+        transform.rotation = Quaternion.identity;
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = new Vector3(transform.position.x - .01f, transform.position.y, transform.position.z);        
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            transform.position = new Vector3(transform.position.x + .01f, transform.position.y, transform.position.z);
+        }
+    }
+
+
+}
